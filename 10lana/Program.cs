9b29a6315623bbd6e6164ca48fce53ced2915fa7
DeadInside <DeadInside@DESKTOP-DH6M68H>
2024-01-25 17:57:37 +0300

    Добавьте файлы проекта.

---
+++ 10lana/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10lana
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+            int c = 7;
+            Console.WriteLine(a + b + c);
+            
+        }
+    }
+}
